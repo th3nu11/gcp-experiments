92e17a82d1f9457e5a9fdb2afc1ee2134a4e419a
carlo.ballabio <carlo.ballabio@intre.it>
2025-08-04 15:47:39 +0200

    new hello world example

---
+++ pipelines/hello-world-docker/index.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+  it('should return Hello World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello World!');
+  });
+});
+
