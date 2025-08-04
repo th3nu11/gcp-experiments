d212a360c1f6ee445ed66f4a19791e35ed2ba8a9
carlo.ballabio <carlo.ballabio@intre.it>
2025-08-04 17:10:48 +0200

    Fix test

--- pipelines/hello-world-docker/index.test.js
+++ pipelines/hello-world-docker/index.test.js
@@ -5,7 +5,6 @@ describe('GET /', () => {
   it('should return Hello World!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World!');
+    expect(res.text.startsWith('Hello World')).toBe(true);
   });
 });
-
